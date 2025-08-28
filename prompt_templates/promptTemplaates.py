abbbcb6e865889304c5ff50753c32ef35a3f3ccc
Mohammed Salih k <salihsha656@gmail.com>
2025-08-28 11:36:12 +0530

    prompt template

--- prompt_templates/promptTemplaates.py
+++ prompt_templates/promptTemplaates.py
@@ -26,16 +26,14 @@ if not os.environ.get("GOOGLE_API_KEY"):
 from langchain.chat_models import init_chat_model
 model=init_chat_model("gemini-2.5-flash",model_provider="google_genai")
 from langchain_core.messages import HumanMessage,SystemMessage
+from langchain_core.prompts import ChatPromptTemplate
 
-messages=[
-    SystemMessage("Translate the following from English to Italian"),
-    HumanMessage("Hi !")
-]
-res=model.invoke(messages)
-res2=model.invoke("hello")
-res3=model.invoke([{"role":"user","content":"Hello"}])
-res4=model.invoke([HumanMessage(content="hello")])
-print(res4)
-
-for token in model.stream(messages):
-    print(token.content,end="|")
+system_template="Translate the following from English into {language}"
+prompt_template=ChatPromptTemplate.from_messages(
+    [("system",system_template),("user","{text}")]
+)
+prompt=prompt_template.invoke({"language":"italian","text":"hii!"})
+# print(prompt)
+# print(prompt.to_messages())
+res=model.invoke(prompt)
+print(res.content)
